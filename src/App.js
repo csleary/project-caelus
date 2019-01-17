dab5fb6618f482c7f396e886fa33274668cfd3bb
Chris <csleary@gmail.com>
2019-01-17 12:27:34 +0100

    Added receiver.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import Receiver from './Receiver';
 import Sage from './Sage';
 import 'normalize.css/normalize.css';
-import style from './App.module.css';
+// import style from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className={style.panel}>
-        <Sage>
-          <Receiver />
-        </Sage>
-        <Sage />
-      </div>
+      // <div className={style.panel}>
+      <Sage>
+        <Receiver />
+      </Sage>
+      // </div>
     );
   }
 }
--- src/Receiver.js
+++ src/Receiver.js
@@ -1,33 +1,124 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import 'libfec';
 import style from './Receiver.module.css';
 
 class Receiver extends Component {
-  state = {
-    isOnline: false,
-    isReceiving: false,
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      droppedFrames: 0,
+      error: '',
+      isOnline: true,
+      isReceiving: false,
+      numReceived: 0,
+      profile: 'ultrasonic',
+      transmission: '',
+    };
+  }
+
+  componentDidMount() {
+    window.Quiet.init({
+      profilesPrefix: `${process.env.PUBLIC_URL}/`,
+      memoryInitializerPrefix: `${process.env.PUBLIC_URL}/`,
+      libfecPrefix: '/',
+    });
+
+    window.Quiet.addReadyCallback(this.onQuietReady, this.onQuietFail);
+  }
+
+  onQuietReady = () => {
+    window.Quiet.receiver({
+      profile: this.state.profile,
+      onReceive: this.onReceive,
+      onCreateFail: this.onReceiverCreateFail,
+      onReceiveFail: this.onReceiveFail,
+    });
+  };
+
+  onQuietFail = reason => {
+    this.setState({ error: `Equipment failure. ${reason}.` });
+  };
+
+  onReceive = payload => {
+    const message = window.Quiet.ab2str(payload);
+    this.setState({
+      error: '',
+      isReceiving: true,
+      numReceived: this.state.numReceived + 1,
+      transmission: this.state.transmission + message,
+    });
+  };
+
+  onReceiverCreateFail = reason => {
+    const createFail = `Interface failure. Reason: "${reason}". Are we able to use your microphone?`;
+    this.setState({ isOnline: false, isReceiving: false, error: createFail });
+  };
+
+  onReceiveFail = numFails => {
+    const receiveFail = `[FRAME DROPPED]`;
+    this.setState({
+      droppedFrames: numFails,
+      error: receiveFail,
+      transmission: this.state.transmission + '[???]',
+    });
   };
 
   handleActivation = () => {
+    window.Quiet.resume();
     this.setState({ isOnline: true });
   };
 
   render() {
-    const { isOnline } = this.state;
+    const {
+      droppedFrames,
+      error,
+      isOnline,
+      numReceived,
+      transmission,
+    } = this.state;
+    const signalQuality = Math.round(
+      (numReceived / (numReceived + droppedFrames)) * 100
+    );
     const status = isOnline ? (
-      'Listening…'
+      <>
+        {`Listening…${error && ' ' + error}`}
+        <br />
+        {!!signalQuality && `Signal quality: ${signalQuality}%`}
+      </>
     ) : (
       <button className={style.activate} onClick={this.handleActivation}>
         Activate?
       </button>
     );
 
+    const printTransmission = transmission
+      .split('\n')
+      .map((line, index, array) => {
+        if (line.length) {
+          return (
+            <Fragment key={index}>
+              {line}
+              {index < array.length - 1 && <br />}
+            </Fragment>
+          );
+        }
+        return null;
+      });
+
     return (
       <>
         <p style={{ textAlign: 'center' }}>
-          --==[SAGE {isOnline ? 'Online' : 'Offline'}]==--
+          {isOnline ? '--==[SAGE Online]==--' : 'SAGE Offline'}
         </p>
         <p>{status}</p>
-        {isOnline && <span className={style.cursor}>_</span>}
+        {isOnline && (
+          <p>
+            {transmission.length > 0 && 'Log: '}
+            {printTransmission}
+            <span className={style.cursor}>_</span>
+          </p>
+        )}
       </>
     );
   }
